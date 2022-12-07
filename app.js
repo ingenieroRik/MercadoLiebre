e11fd4e1a6e7bb693d7e9728c91a1d7ea6bdaa90
Rik <ingeniero_rik@hotmail.com>
2022-12-06 21:45:58 -0300

    antes del profe

--- app.js
+++ app.js
@@ -15,17 +15,15 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'));
 });
 
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname,'/views/register.html'));
+});
 
 
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname,'/views/login.html'));
+});
 /*
-app.get('/babbage', (req, res) => {
-    res.sendFile(path.join(__dirname,'/views/babbage.html'));
-});
-
-app.get('/berners-lee', (req, res) => {
-    res.sendFile(path.join(__dirname,'/views/berners-lee.html'));
-});
-
 app.get('/clarke', (req, res) => {
     res.sendFile(path.join(__dirname,'/views/clarke.html'));
 });
