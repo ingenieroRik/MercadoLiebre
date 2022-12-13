ab3462fec528a829ea7011d5a3b7cec50605aa70
Rik <ingeniero_rik@hotmail.com>
2022-12-13 19:56:07 -0300

    final

--- app.js
+++ app.js
@@ -7,9 +7,15 @@ const app = express();
 
 app.use(express.static('public'))
 
+/*
 app.listen(3030, () =>
 console.log('Servidor corriendo en http://localhost:3030')
 );
+*/
+// levantar nuestro servidor
+const port = process.env.PORT || 3001;
+app.listen(port,() => console.log('Servidor corriendo en el puerto $(port)'));
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'));
@@ -23,24 +29,3 @@ app.get('/register', (req, res) => {
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname,'/views/login.html'));
 });
-/*
-app.get('/clarke', (req, res) => {
-    res.sendFile(path.join(__dirname,'/views/clarke.html'));
-});
-
-app.get('/hamilton', (req, res) => {
-    res.sendFile(path.join(__dirname,'/views/hamilton.html'));
- });
-
-app.get('/hopper', (req, res) => {
-    res.sendFile(path.join(__dirname,'/views/hopper.html'));
-});
-
-app.get('/lovelace', (req, res) => {
-    res.sendFile(path.join(__dirname,'/views/lovelace.html'));
-});
-
-app.get('/turing', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/turing.html'));
-});
-*/
